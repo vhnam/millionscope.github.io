b7ecc60d945d58b4936cb6cb4b4c4c24c91ffe32
Hoang Minh Thanh <hmthanhgm@gmail.com>
2023-05-15 20:35:25 +0700

    update all

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+// const remarkMath = require("remark-math");
+// const rehypeKatex = require("rehype-katex");
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  latex: true,
+  themeConfig: "./theme.config.tsx",
+  mdxOptions: {
+    // remarkPlugins: [remarkMath],
+    // rehypePlugins: [rehypeKatex],
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+// import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>MillionScope</span>,
+  faviconGlyph: "📚",
   project: {
-    link: 'https://github.com/MillionScope/millionscope.github.io',
+    link: "https://github.com/MillionScope/millionscope.github.io",
+    icon: <svg width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path></svg>,
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://twitter.com/millionscope",
   },
-  docsRepositoryBase: 'https://github.com/MillionScope/millionscope.github.io',
+  docsRepositoryBase: "https://github.com/MillionScope/millionscope.github.io",
   footer: {
-    text: 'MillionScope blog',
+    text: "Thanh Minh-Hoang's blog",
   },
-}
+};
 
-export default config
+export default config;
